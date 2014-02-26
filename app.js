ff5ba22c2acaf3b60e7751342dcc12a74199c5af
prescottprue <prescottprue@hotmail.com>
2014-02-26 03:28:27 -0500

    Reorganized and commeted. Titles, descriptions and links added before API request functions.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
  * This requires: restler
  * To install, type 'npm install restler'
  * Tested with node.js v0.6.14
+ * Kimono APIs link (you must me signed in): http://www.kimonolabs.com/apis
+
  */
 
 var util = require('util');
@@ -9,13 +11,16 @@ var restclient = require('restler');
 var request = require('request');
 var parseString = require('xml2js').parseString;
 
-//var fxml_url = 'http://flightxml.flightaware.com/json/FlightXML2/';
+
 var foodery = 'http://www.kimonolabs.com/api/8ab8g67o?apikey=7f8726a3ca95deffb99820d4dc373c91';
-var foodler = 'http://www.kimonolabs.com/api/6ftclxke?apikey=7f8726a3ca95deffb99820d4dc373c91';
-var foodlerTitle = 'http://www.kimonolabs.com/api/cho51hok?apikey=7f8726a3ca95deffb99820d4dc373c91';
-var market = 'http://www.SupermarketAPI.com/api.asmx/';
-//var username = 'YOUR_USERNAME';
-var marketApiKey = '274f79f561';
+
+
+
+/*
+  Foodery Scraper
+  GETs weekley foodery menu using Custom Kimono API "The Foodery Weekly"(refreshes daily).
+  Link: http://www.kimonolabs.com/apis/8ab8g67o
+*/
 request(foodery , function(err, response, body){
   var data = JSON.parse(body);
   var items = data.results.Food;
@@ -25,6 +30,13 @@ request(foodery , function(err, response, body){
     var allergens = items[i].Allergens;
   }
 });
+
+/*
+  General Foodler Scraper
+  GETs all available resturant information using custom Kimono API "Foodler".
+  Link: http://www.kimonolabs.com/apis/2u1eakeo
+*/
+var foodlerTitle = 'http://www.kimonolabs.com/api/cho51hok?apikey=7f8726a3ca95deffb99820d4dc373c91';
 request(foodler, function(err, response, body){
   var data = JSON.parse(body);
   //console.log(data.results.collection1);
@@ -34,8 +46,17 @@ request(foodler, function(err, response, body){
     var picture = items[i].Picture.src;
     var allergens = items[i].Allergens;
     console.log(allergens);
-  }*/
+  }
+  /////Still needs work/////
+  */
 });
+
+/*
+  Foodler Scraper: Resturant titles
+  GETs just resturant titles (more accurate) using custom Kimono API "Foodler Titles". 
+  Link: http://www.kimonolabs.com/apis/cho51hok 
+*/
+var foodler = 'http://www.kimonolabs.com/api/6ftclxke?apikey=7f8726a3ca95deffb99820d4dc373c91';
 var filter = '&'
 request(foodlerTitle + filter, function(err, response, body){
   var data = JSON.parse(body);
@@ -47,6 +68,14 @@ request(foodlerTitle + filter, function(err, response, body){
     console.log(title + ' ' + link);
   }
 });
+
+/*
+  Supermarket Scraper
+  Uses Supermarker API Trial to get grocery store data
+  Link: http://www.supermarketapi.com/Methods_v1.aspx
+*/
+var market = 'http://www.SupermarketAPI.com/api.asmx/';
+var marketApiKey = '274f79f561';
 var command = 'GetGroceries';
 var property = 'SearchText';
 var value = 'Peach';
@@ -58,7 +87,20 @@ request(marketreq, function(err, response, body){
     //console.log(output);
   })
 });
+
+
+
 /*
+------------------------------Not Being Used---------------------------------
+
+//Code for using rest client with google planes API
+
+//Config
+var fxml_url = 'http://flightxml.flightaware.com/json/FlightXML2/';
+var username = 'YOUR_USERNAME';
+var apiKey = 'YOUR_APIKEY';
+
+//Flights at airport: KAUS
 restclient.get(fxml_url + 'MetarEx', {
     username: username,
     password: apiKey,
@@ -68,7 +110,7 @@ restclient.get(fxml_url + 'MetarEx', {
     var entry = result.MetarExResult.metar[0];
     util.puts('The temperature at ' + entry.airport + ' is ' + entry.temp_air + 'C');
 });
-
+//List origin and aircraft type from KIAH
 restclient.get(fxml_url + 'Enroute', {
     username: username,
     password: apiKey,
@@ -83,4 +125,6 @@ restclient.get(fxml_url + 'Enroute', {
       util.puts(flight.ident + ' (' + flight.aircrafttype + ')\t' + 
           flight.originName + ' (' + flight.origin + ')');
     }
-});*/
+});
+------------------------------Not Being Used---------------------------------
+*/
